6fc292f6726e08963751dfd7eedf851682756a46
Jose Miramar <kokoro.miramar@gmail.com>
2014-07-03 18:54:15 -0500

    bugfix conector posgres y mapping table user

--- grails-app/domain/com/app/login/User.groovy
+++ grails-app/domain/com/app/login/User.groovy
@@ -2,47 +2,48 @@ package com.app.login
 
 class User {
 
-	transient springSecurityService
+    transient springSecurityService
         
-        String email
-        String firstName
-        String lastName
-        byte[] imagen
-	String username
-	String password
-	boolean enabled = true
-	boolean accountExpired
-	boolean accountLocked
-	boolean passwordExpired
+    String email
+    String firstName
+    String lastName
+    byte[] imagen
+    String username
+    String password
+    boolean enabled = true
+    boolean accountExpired
+    boolean accountLocked
+    boolean passwordExpired
 
-	static transients = ['springSecurityService']
+    static transients = ['springSecurityService']
 
-	static constraints = {
-		username blank: false, unique: true
-		password blank: false
-                email blank:true, email:true
-                imagen nullable:true
-	}
+    static constraints = {
+        username blank: false, unique: true
+        password blank: false
+        email blank:true, email:true
+        imagen nullable:true
+    }
 
-	static mapping = {
-		password column: '`password`'
-	}
+    static mapping = {            
+        table 'usuario'
+        password column: '`password`'
+    }
 
-	Set<Role> getAuthorities() {
-		UserRole.findAllByUser(this).collect { it.role }
-	}
+    Set<Role> getAuthorities() {
+        UserRole.findAllByUser(this).collect { it.role }
+    }
 
-	def beforeInsert() {
-		encodePassword()
-	}
+    def beforeInsert() {
+        encodePassword()
+    }
 
-	def beforeUpdate() {
-		if (isDirty('password')) {
-			encodePassword()
-		}
-	}
+    def beforeUpdate() {
+        if (isDirty('password')) {
+            encodePassword()
+        }
+    }
 
-	protected void encodePassword() {
-		password = springSecurityService?.passwordEncoder ? springSecurityService.encodePassword(password) : password
-	}
+    protected void encodePassword() {
+        password = springSecurityService?.passwordEncoder ? springSecurityService.encodePassword(password) : password
+    }
 }
